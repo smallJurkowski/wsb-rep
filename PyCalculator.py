79b17bc0cb00020cf54d85bedea465c7ed08bb13
smallJurkowski <157037848+smallJurkowski@users.noreply.github.com>
2024-01-18 16:40:49 +0100

    Update PyCalculator.py

--- PyCalculator.py
+++ PyCalculator.py
@@ -1,1 +1,22 @@
+ def add(x, y):
+  return x + y
+
+def substract(x, y):
+  return x - y
+
+def multiply(x, y):
+  return x * y
+
+def divide(x, y):
+  return x / y
+
+while True:
+  first = float(input("Enter first number: ")
+  symbol = string("Choose symbol '+' '-' '*' '/': ")
+  second = float(input("Enter second number: ")
+
+  if symbol in ('+', '-', '*', '/')
+      if symbol == '+':
+        print add(first, second)
+                 
 
